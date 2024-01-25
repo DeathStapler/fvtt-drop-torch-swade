f207a2f36c50d2a99accb1500135a60fb05014cf
DeathStapler <ray@hodel.com>
2024-01-25 17:24:57 -0500

    Update Light.js

--- src/classes/Light.js
+++ src/classes/Light.js
@@ -1,1 +1,42 @@
+let DEBUG = true;
 
+let debugLog = (...args) => {
+  if (DEBUG) {
+    console.log(...args);
+  }
+};
+
+class Light {
+  static async addTorchButton(hud, hudHtml, hudData) {
+    let actor = game.actors.get(hud.object.document.actorId);
+    
+    let library = await SourceLibrary.load(
+      game.system.id,
+      Settings.lightRadii.bright, 
+      Settings.lightRadii.dim, 
+      Settings.inventoryItemName, 
+      Settings.gameLightSources, 
+      actor.prototypeToken.light,
+    );
+    let token = new TorchToken(hud.object.document, library);
+    let lightSources = token.ownedLightSources;
+
+    // Don't let the tokens we create for light sources have or use their own
+    // light sources recursively.
+    if (hud.object.document.name in lightSources)  return;
+    if (!game.user.isGM && !Settings.playerTorches) return;
+    if (!token.currentLightSource) {
+      TokenHUD.addQueryButton(token, hudHtml)
+      return;
+    }
+    /* Manage torch state */
+    TokenHUD.addFlameButton(
+      token,
+      hudHtml,
+      Torch.forceSourceOff,
+      Torch.toggleLightSource,
+      Torch.toggleLightHeld,
+      Torch.changeLightSource
+    );
+  }
+}
